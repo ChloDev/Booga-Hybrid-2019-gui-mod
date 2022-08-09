e45a96cc9a652dfbf76573c110a293f02a5ead8e
Chlo <106335580+ChloDev@users.noreply.github.com>
2022-08-08 22:00:31 -0400

    Create Gay sex.lua

---
+++ Gay sex.lua
@@ -0,0 +1,395 @@
+local white = Color3.fromRGB(255,255,255)
+local black = Color3.fromRGB(0,0,0)
+local coolpurple = Color3.fromRGB(181,145,222)
+local Red = Color3.fromRGB(155,25,25)
+local Player = game.Players.LocalPlayer
+local MainGui = Player.PlayerGui["MainGui-2019"]
+local LeftPanel = MainGui.LeftPanel
+local RightPanel = MainGui.RightPanel
+local Panels = MainGui.Panels
+
+
+--New Instances for stuff to make it look better
+local UICorner1 = Instance.new("UICorner")
+local UICorner2 = Instance.new("UICorner")
+local UICorner3 = Instance.new("UICorner")
+local UICorner4 = Instance.new("UICorner")
+local UICorner5 = Instance.new("UICorner")--Used use next one
+local UICorner6 = Instance.new("UICorner")---
+local UICorner7 = Instance.new("UICorner")
+local UICorner8 = Instance.new("UICorner")
+local UICorner9 = Instance.new("UICorner")
+local UICorner10 = Instance.new("UICorner")
+local UICorner11 = Instance.new("UICorner")
+local UICorner12 = Instance.new("UICorner")
+local UICorner13 = Instance.new("UICorner")
+local UICorner14 = Instance.new("UICorner")--
+local UICorner15 = Instance.new("UICorner")
+local UICorner16 = Instance.new("UICorner")
+local UICorner17 = Instance.new("UICorner")
+local UICorner18 = Instance.new("UICorner")
+local UICorner19 = Instance.new("UICorner")
+local UICorner20 = Instance.new("UICorner")
+local UICorner21 = Instance.new("UICorner")
+local UICorner22 = Instance.new("UICorner")
+local UICorner23 = Instance.new("UICorner")
+local UICorner24 = Instance.new("UICorner")
+local UICorner25 = Instance.new("UICorner")
+local UICorner26 = Instance.new("UICorner")
+local UICorner27 = Instance.new("UICorner")
+local UICorner28 = Instance.new("UICorner")
+local UICorner29 = Instance.new("UICorner")
+local UICorner30 = Instance.new("UICorner")
+local UICorner31 = Instance.new("UICorner")
+local UICorner32 = Instance.new("UICorner")--------
+local UICorner33 = Instance.new("UICorner")
+local UICorner34 = Instance.new("UICorner")
+local UICorner35 = Instance.new("UICorner")
+local UICorner36 = Instance.new("UICorner")
+local UICorner37 = Instance.new("UICorner")
+local UICorner38 = Instance.new("UICorner")
+local UICorner39 = Instance.new("UICorner")
+local UICorner40 = Instance.new("UICorner")
+local UICorner41 = Instance.new("UICorner")
+local UICorner42 = Instance.new("UICorner")
+local UICorner43 = Instance.new("UICorner")
+local UICorner44 = Instance.new("UICorner")
+local UICorner45 = Instance.new("UICorner")
+local UICorner46 = Instance.new("UICorner")
+local UICorner47 = Instance.new("UICorner")
+local UICorner48 = Instance.new("UICorner")
+local UICorner49 = Instance.new("UICorner")
+local UICorner50 = Instance.new("UICorner")
+local UICorner51 = Instance.new("UICorner")
+local UICorner52 = Instance.new("UICorner")
+local UICorner53 = Instance.new("UICorner")
+local UICorner54 = Instance.new("UICorner")
+local UICorner55 = Instance.new("UICorner")
+local UICorner56 = Instance.new("UICorner")
+local UICorner57 = Instance.new("UICorner")
+local UICorner58 = Instance.new("UICorner")
+local UICorner59 = Instance.new("UICorner")
+local UICorner60 = Instance.new("UICorner")
+local UICorner61 = Instance.new("UICorner")
+local UICorner62 = Instance.new("UICorner")
+local UICorner63 = Instance.new("UICorner")
+local UICorner64 = Instance.new("UICorner")
+local UICorner65 = Instance.new("UICorner")
+local UICorner66 = Instance.new("UICorner")
+local UICorner67 = Instance.new("UICorner")
+local UICorner68 = Instance.new("UICorner")
+local UICorner69 = Instance.new("UICorner")
+local UICorner70 = Instance.new("UICorner")
+local UICorner71 = Instance.new("UICorner")
+local UICorner72 = Instance.new("UICorner")
+local UICorner73 = Instance.new("UICorner")
+local UICorner74 = Instance.new("UICorner")
+local UICorner75 = Instance.new("UICorner")
+local UICorner76 = Instance.new("UICorner")
+local UICorner77 = Instance.new("UICorner")
+local UICorner78 = Instance.new("UICorner")
+local UICorner79 = Instance.new("UICorner")
+local UICorner80 = Instance.new("UICorner")
+local UICorner81 = Instance.new("UICorner")
+local UICorner82 = Instance.new("UICorner")
+local UICorner83 = Instance.new("UICorner")
+local UICorner84 = Instance.new("UICorner")
+local UICorner85 = Instance.new("UICorner")
+local UICorner86 = Instance.new("UICorner")
+local UICorner87 = Instance.new("UICorner")
+local UICorner88 = Instance.new("UICorner")
+local UICorner89 = Instance.new("UICorner")
+local UICorner90 = Instance.new("UICorner")
+local UICorner91 = Instance.new("UICorner")
+local UICorner92 = Instance.new("UICorner")
+local UICorner93 = Instance.new("UICorner")
+local UICorner94 = Instance.new("UICorner")
+local UICorner95 = Instance.new("UICorner")
+local UICorner96 = Instance.new("UICorner")
+local UICorner97 = Instance.new("UICorner")
+local UICorner98 = Instance.new("UICorner")
+local UICorner99 = Instance.new("UICorner")
+local UICorner100 = Instance.new("UICorner")
+-- font is "GothamBlack"
+
+--print("lol")
+ RightPanel.Inventory.Templates.ItemFrame.BackgroundColor3 = black
+ RightPanel.Inventory.Templates.ItemFrame.BackgroundTransparency = .8
+ RightPanel.Inventory.Templates.ItemFrame.ImageTransparency = 1
+ RightPanel.Inventory.Templates.ItemFrame.Title.TextColor3 = white
+ RightPanel.Inventory.Templates.ItemFrame.Title.Font = "GothamBlack"
+ RightPanel.Inventory.Templates.ItemFrame.QuantityImage.QuantityText.Font = "GothamBlack"
+ RightPanel.Inventory.Templates.ItemFrame.QuantityImage.QuantityText.TextStrokeTransparency = 1
+  RightPanel.Inventory.Templates.ItemFrame.QuantityImage.QuantityText.Size = UDim2.new(2.514,0,.703,0)
+   RightPanel.Inventory.Templates.ItemFrame.QuantityImage.QuantityText.Position = UDim2.new(.764,0,0.75,0)
+RightPanel.Inventory.List.UIGridLayout.CellPadding = UDim2.new(0,3,0,3)
+ RightPanel.Inventory.Templates.ItemFrame.QuantityImage.ImageTransparency = 1
+UICorner6.Parent = RightPanel.Inventory.Templates.ItemFrame
+UICorner6.CornerRadius = UDim.new(0,15)
+
+Panels.MouseFrame.Title.Font = "GothamBlack"
+Panels.MouseFrame.Title.TextStrokeTransparency = .8
+
+Panels.MouseFrame.Pickup.Font = "GothamBlack"
+Panels.MouseFrame.Pickup.TextStrokeTransparency = .8
+
+Panels.MouseFrame.Consume.Font = "GothamBlack"
+Panels.MouseFrame.Consume.TextStrokeTransparency = .8
+
+RightPanel.Inventory.ArmorFrame.BackgroundTransparency = .75
+RightPanel.Inventory.ArmorFrame.TextLabel.BackgroundTransparency = 1
+UICorner37.Parent = RightPanel.Inventory.ArmorFrame
+UICorner37.CornerRadius = UDim.new(0,15)
+
+
+LeftPanel.Craft.SearchBar.BackgroundColor3 = black
+LeftPanel.Craft.SearchBar.BorderSizePixel = 0 
+LeftPanel.Craft.SearchBar.BackgroundTransparency = .5
+UICorner33.Parent = LeftPanel.Craft.SearchBar
+UICorner33.CornerRadius = UDim.new(0,25)
+LeftPanel.Craft.Backdrop.ImageTransparency = 1 
+LeftPanel.Craft.Backdrop.BackgroundColor3 = black
+LeftPanel.Craft.Backdrop.BackgroundTransparency = .5
+UICorner34.Parent = LeftPanel.Craft.Backdrop
+UICorner34.CornerRadius = UDim.new(0,15)
+
+RightPanel.Inventory.Backdrop.ImageTransparency = 1
+RightPanel.Inventory.Backdrop.BackgroundTransparency = .5
+RightPanel.Inventory.Backdrop.BackgroundColor3 = black
+UICorner38.Parent = RightPanel.Inventory.Backdrop
+UICorner38.CornerRadius = UDim.new(0,15)
+
+RightPanel.Inventory.Selection.All.ActiveImage.ImageColor3 = black
+RightPanel.Inventory.Selection.All.ActiveImage.ImageTransparency = .5
+
+RightPanel.Inventory.Selection.Tools.ActiveImage.ImageColor3 = black
+RightPanel.Inventory.Selection.Tools.ActiveImage.ImageTransparency = .5
+
+RightPanel.Inventory.Selection.Food.ActiveImage.ImageColor3 = black
+RightPanel.Inventory.Selection.Food.ActiveImage.ImageTransparency = .5
+
+RightPanel.Inventory.Selection.Objects.ActiveImage.ImageColor3 = black
+RightPanel.Inventory.Selection.Objects.ActiveImage.ImageTransparency = .5
+
+RightPanel.Inventory.Selection.Armor.ActiveImage.ImageColor3 = black
+RightPanel.Inventory.Selection.Armor.ActiveImage.ImageTransparency = .5
+
+
+LeftPanel.Craft.Selection.All.ActiveImage.ImageTransparency = .5
+LeftPanel.Craft.Selection.All.ActiveImage.ImageColor3 = black
+
+LeftPanel.Craft.Selection.Tools.ActiveImage.ImageTransparency = .5
+LeftPanel.Craft.Selection.Tools.ActiveImage.ImageColor3 = black
+
+LeftPanel.Craft.Selection.Buildings.ActiveImage.ImageTransparency = .5
+LeftPanel.Craft.Selection.Buildings.ActiveImage.ImageColor3 = black
+
+LeftPanel.Craft.Selection.Boats.ActiveImage.ImageTransparency = .5
+LeftPanel.Craft.Selection.Boats.ActiveImage.ImageColor3 = black
+
+LeftPanel.Craft.Selection.Armor.ActiveImage.ImageTransparency = .5
+LeftPanel.Craft.Selection.Armor.ActiveImage.ImageColor3 = black
+
+
+
+
+
+
+ LeftPanel.Craft.Templates.LockedFrame.LockedLabel.Font = "GothamBlack"
+ LeftPanel.Craft.Templates.LockedFrame.SecretTitle.Font = "GothamBlack"
+ 
+ print("EEbj")
+ RightPanel.Inventory.ArmorFrame.TextLabel.TextStrokeTransparency = 1
+ RightPanel.Inventory.ArmorFrame.TextLabel.Font = "GothamBlack"
+  print("EEz")
+ RightPanel.Inventory.ArmorFrame.Selection.bag.ActiveIcon.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.bag.ActiveIcon.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.bag.EmptyFrame.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.bag.EmptyFrame.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.bag.DefaultIcon.ImageColor3 = white
+ RightPanel.Inventory.ArmorFrame.Selection.bag.EmptyFrame.ImageTransparency = 0
+ print("EEa")
+ RightPanel.Inventory.ArmorFrame.Selection.head.ActiveIcon.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.head.ActiveIcon.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.head.EmptyFrame.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.head.EmptyFrame.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.head.DefaultIcon.ImageColor3 = white
+ RightPanel.Inventory.ArmorFrame.Selection.head.EmptyFrame.ImageTransparency = 0
+ print("EEb")
+ RightPanel.Inventory.ArmorFrame.Selection.legs.ActiveIcon.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.legs.ActiveIcon.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.legs.EmptyFrame.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.legs.EmptyFrame.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.legs.DefaultIcon.ImageColor3 = white
+ RightPanel.Inventory.ArmorFrame.Selection.legs.EmptyFrame.ImageTransparency = 0
+ print("EEc")
+ RightPanel.Inventory.ArmorFrame.Selection.arms.ActiveIcon.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.arms.ActiveIcon.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.arms.EmptyFrame.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.arms.EmptyFrame.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.arms.DefaultIcon.ImageColor3 = white
+ RightPanel.Inventory.ArmorFrame.Selection.arms.EmptyFrame.ImageTransparency = 0
+ print("EEd")
+ RightPanel.Inventory.ArmorFrame.Selection.torso.ActiveIcon.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.torso.ActiveIcon.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.torso.EmptyFrame.ImageColor3 = black
+ RightPanel.Inventory.ArmorFrame.Selection.torso.EmptyFrame.ImageTransparency = .5
+ RightPanel.Inventory.ArmorFrame.Selection.torso.DefaultIcon.ImageColor3 = white
+ RightPanel.Inventory.ArmorFrame.Selection.torso.EmptyFrame.ImageTransparency = 0
+ print("EEe")
+ RightPanel.Inventory.Backdrop.Image = ""
+
+ LeftPanel.Craft.Backdrop.Image = ""
+
+
+ --RightPanel.Inventory.BagMeter.ImageLabel.ImageColor3 = coolpurple
+ --RightPanel.Inventory.BagMeter.Slider.BackgroundColor3 = coolpurple
+ print("EE")
+LeftPanel.Craft.Templates.Ingredient.BackgroundColor3 = black
+LeftPanel.Craft.Templates.Ingredient.BackgroundTransparency = .8
+LeftPanel.Craft.Templates.Ingredient.ImageTransparency = 1
+UICorner8.Parent = LeftPanel.Craft.Templates.Ingredient
+UICorner8.CornerRadius = UDim.new(0,10)
+
+
+RightPanel.Inventory.List.Size = UDim2.new(1,0,.843,0)
+RightPanel.Inventory.List.Position = UDim2.new(0,0,.157,0)
+
+ 
+
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.CanCraftImage.BackgroundColor3 = coolpurple
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.CanCraftImage.Image = 0
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.CanCraftImage.BackgroundTransparency = 0
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.CanCraftImage.TextLabel.TextScaled = true
+UICorner1.Parent =  LeftPanel.Craft.Templates.ItemFrame.CraftButton.CanCraftImage
+UICorner1.CornerRadius = UDim.new(0,15)
+
+
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage.BackgroundColor3 = Red
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage.Image = 0
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage.BackgroundTransparency = 0
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage.TextLabel.Font = "GothamBlack"
+ LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage.TextLabel.TextScaled = true
+UICorner2.Parent =  LeftPanel.Craft.Templates.ItemFrame.CraftButton.NoCraftImage
+UICorner2.CornerRadius = UDim.new(0,15)
+
+LeftPanel.Craft.Templates.ItemFrame.NameLabel.Font = "GothamBlack"
+
+LeftPanel.Craft.Templates.ItemFrame.ItemIconBackdrop.BackgroundColor3 = black
+LeftPanel.Craft.Templates.ItemFrame.ItemIconBackdrop.BackgroundTransparency = .8
+LeftPanel.Craft.Templates.ItemFrame.ItemIconBackdrop.ImageTransparency = 1
+UICorner7.Parent = LeftPanel.Craft.Templates.ItemFrame.ItemIconBackdrop
+UICorner7.CornerRadius = UDim.new(0,10)
+
+LeftPanel.Craft.Templates.ItemFrame.BackgroundColor3 = black
+LeftPanel.Craft.Templates.ItemFrame.BackgroundTransparency = .8
+LeftPanel.Craft.Templates.ItemFrame.ImageTransparency = 1
+UICorner12.Parent = LeftPanel.Craft.Templates.ItemFrame
+UICorner12.CornerRadius = UDim.new(0,10)
+
+LeftPanel.Craft.SearchBar.Font = "GothamBlack"
+LeftPanel.Craft.SearchBar.PlaceholderText = "Search..."
+LeftPanel.Craft.SearchBar.TextScaled = true
+
+LeftPanel.Craft.List.UIListLayout.Padding = UDim.new(0,5)
+LeftPanel.Craft.Templates.ItemFrame.Size = UDim2.new(.96,0,1,0)
+LeftPanel.Craft.List.UIListLayout.HorizontalAlignment = "Left"
+
+
+
+
+Panels.Toolbar.List["1"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["1"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["1"].ActiveIcon.BackgroundTransparency = .3
+UICorner62.Parent = Panels.Toolbar.List["1"].ActiveIcon
+UICorner62.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["1"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["1"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["1"].InactiveIcon.BackgroundTransparency = .5
+UICorner63.Parent = Panels.Toolbar.List["1"].InactiveIcon
+UICorner63.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["1"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["1"].Quantity.TextStrokeTransparency = 1
+
+Panels.Toolbar.List["2"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["2"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["2"].ActiveIcon.BackgroundTransparency = .3
+UICorner64.Parent = Panels.Toolbar.List["2"].ActiveIcon
+UICorner64.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["2"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["2"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["2"].InactiveIcon.BackgroundTransparency = .5
+UICorner65.Parent = Panels.Toolbar.List["2"].InactiveIcon
+UICorner65.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["2"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["2"].Quantity.TextStrokeTransparency = 1
+
+Panels.Toolbar.List["3"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["3"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["3"].ActiveIcon.BackgroundTransparency = .3
+UICorner66.Parent = Panels.Toolbar.List["3"].ActiveIcon
+UICorner66.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["3"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["3"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["3"].InactiveIcon.BackgroundTransparency = .5
+UICorner67.Parent = Panels.Toolbar.List["3"].InactiveIcon
+UICorner67.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["3"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["3"].Quantity.TextStrokeTransparency = 1
+
+Panels.Toolbar.List["4"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["4"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["4"].ActiveIcon.BackgroundTransparency = .3
+UICorner68.Parent = Panels.Toolbar.List["4"].ActiveIcon
+UICorner68.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["4"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["4"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["4"].InactiveIcon.BackgroundTransparency = .5
+UICorner69.Parent = Panels.Toolbar.List["4"].InactiveIcon
+UICorner69.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["4"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["4"].Quantity.TextStrokeTransparency = 1
+
+Panels.Toolbar.List["5"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["5"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["5"].ActiveIcon.BackgroundTransparency = .3
+UICorner70.Parent = Panels.Toolbar.List["5"].ActiveIcon
+UICorner70.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["5"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["5"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["5"].InactiveIcon.BackgroundTransparency = .5
+UICorner71.Parent = Panels.Toolbar.List["5"].InactiveIcon
+UICorner71.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["5"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["5"].Quantity.TextStrokeTransparency = 1
+
+Panels.Toolbar.List["6"].ActiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["6"].ActiveIcon.BackgroundColor3 = coolpurple
+Panels.Toolbar.List["6"].ActiveIcon.BackgroundTransparency = .3
+UICorner72.Parent = Panels.Toolbar.List["6"].ActiveIcon
+UICorner72.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["6"].InactiveIcon.ImageTransparency = 1
+Panels.Toolbar.List["6"].InactiveIcon.BackgroundColor3 = black
+Panels.Toolbar.List["6"].InactiveIcon.BackgroundTransparency = .5
+UICorner73.Parent = Panels.Toolbar.List["6"].InactiveIcon
+UICorner73.CornerRadius = UDim.new(0,15)
+Panels.Toolbar.List["6"].Quantity.Font = "GothamBlack"
+Panels.Toolbar.List["6"].Quantity.TextStrokeTransparency = 1
+
+
+MainGui.Panels.Toolbar.FarmFrame.Templates.ImageButton.BackgroundColor3 = black
+MainGui.Panels.Toolbar.FarmFrame.Templates.ImageButton.Transparency = .8
+MainGui.Panels.Toolbar.FarmFrame.Templates.ImageButton.TextLabel.TextColor3 = white
+MainGui.Panels.Toolbar.FarmFrame.Templates.ImageButton.TextLabel.Font = "GothamBlack"
+for i,v in pairs(game:GetService("Players").SoybeenNotStraight.PlayerGui["MainGui-2019"].Panels.Toolbar.FarmFrame.List:GetChildren()) do
+    if v.ClassName == "ImageButton" then
+        v.BackgroundColor3 = black
+        v.Transparency = .8
+        v.TextLabel.TextColor3 = white
+                v.TextLabel.Font = "GothamBlack"
+        end
+    end
+
+
+
+
